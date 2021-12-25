da60300c4d3e376942258409b9a432f734be4498
fengyuwusuong <602766695@qq.com>
2021-12-25 23:11:38 +0800

    feat: finish orm day7-migrate

---
+++ orm/day7-migrate/geeorm.go
@@ -0,0 +1,130 @@
+package geeorm
+
+import (
+	"database/sql"
+	"fmt"
+	"geeorm/dialect"
+	"geeorm/log"
+	"geeorm/session"
+	"strings"
+)
+
+type Engine struct {
+	db      *sql.DB
+	dialect dialect.Dialect
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %v not found", driver)
+		return
+	}
+	e = &Engine{db: db, dialect: dial}
+	log.Info("Connect database success")
+	return
+}
+
+func (engine *Engine) Close() {
+	if err := engine.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db, engine.dialect)
+}
+
+type TxFunc func(*session.Session) (interface{}, error)
+
+func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := engine.NewSession()
+	if err := s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			// rollback失败err不返回
+			_ = s.Rollback() // err is non-nil; don't change it
+		} else {
+			// commit 失败仍需要rollback
+			defer func() {
+				if err != nil {
+					_ = s.Rollback()
+				}
+			}()
+			err = s.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return f(s)
+}
+
+// difference returns a - b
+func difference(a []string, b []string) (diff []string) {
+	mapB := make(map[string]bool)
+	for _, v := range b {
+		mapB[v] = true
+	}
+	for _, v := range a {
+		if _, ok := mapB[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return
+}
+
+// Migrate table
+func (engine *Engine) Migrate(value interface{}) error {
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+
+		// 获取两个表互相的差集 并输出需要增加和删除的字段
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.FieldNames, columns)
+		delCols := difference(columns, table.FieldNames)
+		log.Infof("added cols %v, deleted cols %v", addCols, delCols)
+
+		// 将新数据结构增加字段加到已有表中
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", table.Name, f.Name, f.Type)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		if len(delCols) == 0 {
+			return
+		}
+
+		// 创建临时表仅迁移新对象相关字段
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FieldNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s from %s;", tmp, fieldStr, table.Name))
+		// 删除旧表并重命名新表
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;", tmp, table.Name))
+		_, err = s.Exec()
+		return
+	})
+	return err
+}
